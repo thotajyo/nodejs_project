cd1084b30ce514f6746e50b71ae6ead285463c3f
thotajyo <129952140+thotajyo@users.noreply.github.com>
2024-08-22 21:22:52 +0530

    Update main.tf

--- eks_terraform/main.tf
+++ eks_terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
  #Creating IAM role for EKS
   resource "aws_iam_role" "master" {
-    name = "jyothi-eks-master"
+    name = "jyothi-eks-master1"
 
     assume_role_policy = jsonencode({
       "Version": "2012-10-17",
@@ -36,7 +36,7 @@ provider "aws" {
   }
 
   resource "aws_iam_role" "worker" {
-    name = "jyothi-eks-worker"
+    name = "jyothi-eks-worker1"
 
     assume_role_policy = jsonencode({
       "Version": "2012-10-17",
@@ -53,7 +53,7 @@ provider "aws" {
   }
 
   resource "aws_iam_policy" "autoscaler" {
-    name = "jyothi-eks-autoscaler-policy"
+    name = "jyothi-eks-autoscaler-policy1"
     policy = jsonencode({
       "Version": "2012-10-17",
       "Statement": [
@@ -106,7 +106,7 @@ provider "aws" {
 
   resource "aws_iam_instance_profile" "worker" {
     depends_on = [aws_iam_role.worker]
-    name       = "jyothi-eks-worker-new-profile"
+    name       = "jyothi-eks-worker-new-profile1"
     role       = aws_iam_role.worker.name
   }
  
